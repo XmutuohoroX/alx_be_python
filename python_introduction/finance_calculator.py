7dbd7bb81ec1534d8a9bb6e9a7d78aebf8081fd6
XmutuohoroX <mutuohorochege@outlook.com>
2025-11-25 22:50:59 +0300

    add script for calculating total yearly savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_savings = monthly_income - monthly_expenses
+print("Your monthly savings are", monthly_savings)
+print("Projected savings after one year, with interest, is:", monthly_savings *12 + (monthly_savings *12 * 0.05))
